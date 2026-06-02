a066aa8749d91f34881ded91f82078e487cf8243
Siya Nayak <siya110410@gmail.com>
2026-06-02 08:12:09 -0400

    Did some of the script while figuring what worked and what didn't

---
+++ game.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@onready var ball= $Ball
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randBall()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+
+func _on_ball_mouse_entered() -> void:
+	randBall()
+
+func randBall():
+	ball.position = Vector2(randi_range(0, 1152),randi_range(0,648))
