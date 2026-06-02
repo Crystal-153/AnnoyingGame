2e7133a8113c867734a01a7d30586949a6722492
Siya Nayak <siya110410@gmail.com>
2026-06-02 09:05:49 -0400

    last commit i think

---
+++ end_game.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var button= $Button
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_mouse_entered() -> void:
+	randbutton()
+
+func randbutton():
+	button.position=Vector2(randi_range(0,1152),randi_range(0,648))
--- game.gd
+++ game.gd
@@ -1,9 +1,10 @@
 extends Node
 
 @onready var ball= $Ball
+@onready var time=true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	randBall()
+	ball.position = Vector2(300, 300)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +17,14 @@ func _on_ball_mouse_entered() -> void:
 	randBall()
 
 func randBall():
-	ball.position = Vector2(randi_range(0, 1152),randi_range(0,648))
-
+	if time==true:
+		ball.position = Vector2(randi_range(0, 1152),randi_range(0,648))
+	else:
+		pass
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	time=false
+
+
+func _on_ball_pressed() -> void:
+	get_tree().change_scene_to_file("res://end_game.tscn")
